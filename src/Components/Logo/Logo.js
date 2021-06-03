1b3179cfe546f771f30ccd36e66b60bbdbc9e944
Ragaventhan Siva <36502965+RagavanRaja@users.noreply.github.com>
2021-06-03 22:52:23 +0530

    Create Logo.js

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import burgerLogo from "../../assets/images/28.1 burger-logo.png";
+import classes from "./Logo.module.css";
+
+const logo = (props) => (
+  <div className={classes.Logo} style={{height:props.height}}>
+    <img src={burgerLogo} alt="My Burger" />
+  </div>
+);
+export default logo;
